1a46d40773b375da92be0828962db5b12a6dea1d
טל בנו <talbeno123@gmail.com>
2021-06-30 00:14:55 +0300

    update everything

--- AI.js
+++
@@ -1,1 +0,0 @@
-//AI code will go here
--- main.js
+++ main.js
@@ -1,80 +1,111 @@
-let mainBoard = new Array(8);
+class Cell {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    isHeld = false;
+    isKing = false;
+}
+
+let turn = "R";
+
+let pieces = [];
+
 for (let i = 0; i < 8; i++) {
-    mainBoard[i] = new Array(8);
     for (let j = 0; j < 8; j++) {
-        mainBoard[i][j] = new Cell("", j, i);
-        if ((i + j) % 2 == 1 && i < 3) {
-            mainBoard[i][j].color = "Black";
-        }
-        if ((i + j) % 2 == 1 && i > 4) {
-            mainBoard[i][j].color = "Red";
+        if ((j + i) % 2 == 1) {
+            if (i < 3) {
+                pieces.push(new Cell(j, i, "B"));
+            }
+            if (i >= 5) {
+                pieces.push(new Cell(j, i, "R"));
+            }
         }
     }
 }
 
-let turn = "Red";
 let cellSize;
-let redCrown;
-let blackCrown;
-
-function preload() {
-    redCrown = loadImage('https://i.imgur.com/tPV4WvM.png');
-    blackCrown = loadImage('https://i.imgur.com/3VxLqnA.png');
-}
 
 function setup() {
-    createCanvas(min(windowWidth, windowHeight), min(windowWidth, windowHeight));
-    cellSize = width / 8;
+    createCanvas(windowHeight, windowHeight);
+    cellSize = windowHeight / 8;
 }
 
 function draw() {
     background(255);
-    stroke(0);
-    strokeWeight(4);
-    line(width, 0, width, height);
-    line(0, height, width, height);
+
+    strokeWeight(0);
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            if ((i + j) % 2 == 1) {
-                fill(0);
-                strokeWeight(0);
+            if ((j + i) % 2 == 1) {
+                fill(0)
                 square(j * cellSize, i * cellSize, cellSize);
             }
-            if (mainBoard[i][j].color != "") {
-                strokeWeight((mainBoard[i][j].isSelected) ? (4) : (2));
-                stroke((mainBoard[i][j].isSelected) ? ("#0000FF") : ("FFFFFF"));
-                fill((mainBoard[i][j].color == "Red") ? (255) : (0), 0, 0);
-                if (mainBoard[i][j].isNextMove) {
-                    if (mainBoard[i][j].color == "Red") {
-                        fill(255, 0, 0, 100);
-                        stroke(0);
-                    }
-                    if (mainBoard[i][j].color == "Black") {
-                        fill(100, 200);
-                        //stroke(255);
-                    }
-                }
-                circle((j + 0.5) * cellSize, (i + 0.5) * cellSize, 0.9 * cellSize);
-                if (mainBoard[i][j].isKing) {
-                    image((mainBoard[i][j].color == "Red") ? (redCrown) : (blackCrown), (j + 0.15) * cellSize, (i + 0.15) * cellSize, 0.7 * cellSize, 0.7 * cellSize);
-                }
+        }
+    }
+
+    strokeWeight(3);
+    stroke(255);
+    let hold = pieces.find(piece => piece.isHeld);
+    pieces.forEach(piece => {
+        if (!piece.isHeld) {
+            if (piece.color == "R") {
+                fill(255, 0, 0);
+            } else if (piece.color == "B") {
+                fill(0);
             }
+            circle((piece.x + 0.5) * cellSize, (piece.y + 0.5) * cellSize, 0.9 * cellSize);
+        }
+    });
+
+    if (hold != undefined) {
+        if (hold.color == "R") {
+            fill(255, 0, 0);
+        } else if (hold.color == "B") {
+            fill(0);
+        }
+        circle(mouseX, mouseY, cellSize);
+    }
+}
+
+function touchStarted() {
+    let x = floor(mouseX / cellSize);
+    let y = floor(mouseY / cellSize);
+    if ((x + y) % 2 == 1) {
+        let piece = pieces.find(piece => piece.x == x && piece.y == y);
+        if (turn == piece.color) {
+            piece.isHeld = true;
         }
     }
 }
 
-function mousePressed() {
-    let i = floor(mouseY / cellSize);
-    let j = floor(mouseX / cellSize);
-    if (mouseButton === RIGHT) {
-        mainBoard[i][j].color = "Black";
-    } else {
-        //console.log(mainBoard[i][j]);
-        if (mainBoard[i][j].color == turn) {
-            if (mainBoard[i][j].isNextMove) {
-                turn = (turn == "Red") ? ("Black") : ("Red");
+function touchEnded() {
+    let x = floor(mouseX / cellSize);
+    let y = floor(mouseY / cellSize);
+    let heldPiece = pieces.find(piece => piece.isHeld);
+    let over = pieces.find(piece => piece.x == x && piece.y == y);
+    if (heldPiece != undefined) {
+        let dir = (heldPiece.color != "R");
+        if ((x + y) % 2 == 1 && over == undefined && (Math.abs(heldPiece.x - x) == Math.abs(heldPiece.y - y))) {
+            if (!heldPiece.isKing && (heldPiece.y - y < 0) == dir) {
+                if (Math.abs(heldPiece.x - x) == 1) {
+                    heldPiece.x = x;
+                    heldPiece.y = y;
+                    turn = (turn == "R") ? ("B") : ("R");
+                } else if (Math.abs(heldPiece.x - x) == 2) {
+                    let eaten = pieces.find(middle => middle.x == (heldPiece.x + x) / 2 && middle.y == (heldPiece.y + y) / 2);
+                    if (eaten != undefined) {
+                        if (eaten.color != heldPiece.color) {
+                            heldPiece.x = x;
+                            heldPiece.y = y;
+                            turn = (turn == "R") ? ("B") : ("R");
+                            pieces.splice(pieces.indexOf(eaten), 1);
+                        }
+                    }
+                }
             }
-            mainBoard[i][j].choose(mainBoard, turn);
         }
+        heldPiece.isHeld = false;
     }
 }
